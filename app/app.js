11b3558b4cbaf21355d79a24c8802033c4d6161d
Lucas Conti <conty.lucas@gmail.com>
2018-02-14 17:52:10 -0200

    Innitial Commit

--- app/app.js
+++ app/app.js
@@ -3,8 +3,11 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'ui.bootstrap',
+  'firebase',
   'myApp.view1',
   'myApp.view2',
+  'myApp.view3',
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
